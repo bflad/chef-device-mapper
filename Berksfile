ef412fb67509e0f0f4e8bcf53828768c07eaee7a
Brian Flad <bflad417@gmail.com>
2014-04-07 20:52:07 -0400

    Rubocop automatic fixes

--- Berksfile
+++ Berksfile
@@ -3,6 +3,6 @@ site :opscode
 metadata
 
 group :integration do
-  cookbook "minitest-handler"
-  cookbook "device-mapper_test", path: "test/cookbooks/device-mapper_test"
+  cookbook 'minitest-handler'
+  cookbook 'device-mapper_test', path: 'test/cookbooks/device-mapper_test'
 end
--- metadata.rb
+++ metadata.rb
@@ -1,13 +1,13 @@
-name              'device-mapper'
-maintainer        'Brian Flad'
-maintainer_email  'bflad417@gmail.com'
-license           'Apache 2.0'
-description       'Installs/Configures device-mapper'
-long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '0.1.0'
-recipe            'device-mapper', 'Installs/Configures device-mapper'
-recipe            'device-mapper::module', 'Loads device-mapper Linux module'
-recipe            'device-mapper::package', 'Installs device-mapper via package'
+name 'device-mapper'
+maintainer 'Brian Flad'
+maintainer_email 'bflad417@gmail.com'
+license 'Apache 2.0'
+description 'Installs/Configures device-mapper'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '0.1.0'
+recipe 'device-mapper', 'Installs/Configures device-mapper'
+recipe 'device-mapper::module', 'Loads device-mapper Linux module'
+recipe 'device-mapper::package', 'Installs device-mapper via package'
 
 supports 'centos', '>= 6.0'
 supports 'debian', '>= 7.0'
